72b11f53a7f961a9dd0d864df85852422990c965
Maurot31 <maurotoledo031@gmail.com>
2023-10-10 19:16:18 -0300

    Add: Headroom, iconos a cards

--- src/components/cardSectionIndex/CardListComunidad.js
+++ src/components/cardSectionIndex/CardListComunidad.js
@@ -1,25 +1,31 @@
 import CardComunidad from "./CardComunidad"
+import { MdHighQuality, MdOutlineDeliveryDining, MdAttachMoney } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
 
 const infoComunidad = [
     {
         id: 1,
         title: "Nuestra calidad",
-        text: "Contamos con la mejor calidad en productos, nuestras remeras están confeccionadas en 100% algodón"
+        text: "Contamos con la mejor calidad en productos, nuestras remeras están confeccionadas en 100% algodón",
+        icon: <MdHighQuality size={'6rem'} color={'#6b6868'}/>
     },
     {
         id: 2,
         title: "Formas de entrega",
-        text: "Podés retirar sin cargo en cualquiera de nuestras tiendas, u optar por envío a domicilio con un costo"
+        text: "Podés retirar sin cargo en cualquiera de nuestras tiendas, u optar por envío a domicilio con un costo",
+        icon: <MdOutlineDeliveryDining size={'6rem'} color={'#6b6868'}/>
     },
     {
         id: 3,
         title: "Comunidad Heroe",
-        text: "Suscribite a nuestro newsletter y obtené los mejores descuentos en nuestra tienda online, Te estamos esperando"
+        text: "Suscribite a nuestro newsletter y obtené los mejores descuentos en nuestra tienda online, Te estamos esperando",
+        icon: <FaUsers size={'6rem'} color={'#6b6868'}/>
     },
     {
         id: 4,
         title: "Formas de pago",
-        text: "Aboná con todos los medios de pago de Mercado Pago o pagá en eftvo al momento de retirar en cualquiera de nuestras sucursales"
+        text: "Aboná con todos los medios de pago de Mercado Pago o pagá en eftvo al momento de retirar en cualquiera de nuestras sucursales",
+        icon: <MdAttachMoney size={'6rem'} color={'#6b6868'}/>
     }
 ]
 
