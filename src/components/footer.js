f1d6814626e9537d1445e9f6bfd29c9bc655fa15
JuanCruzCadoppi <juancruzcadoppi03rhotmail.com>
2023-10-11 16:40:01 -0300

    actualizacion footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,47 @@
 import React from "react";
-import "../styles/footer.css";
 
 const Footer = ()=>{
     return (
         <div className="contenedor-footer">
-            <div className="caja">          
-                
+            <div className="contenedor-1">
+                <div className="caja">          
+                    <img  src='../img/Logo.png' className='imagen-footer' alt='Logo de la Pagina' />
+                </div>
+                <div className="caja">
+                    <h2>SOBRE NOSOTROS</h2>
+                    <p>Somos una tienda online que se dedica a vender remeras de diferentes personajes de comics y demas estilos </p>
+                </div>
+                <div className="caja">
+                    <h2>SIGUENOS</h2>
+                    <div className="red-social">
+                        <a href='#' className="icono-facebook"> </a>
+                        <a href='#' className="icono-instagram"> </a>
+                        <a href='#' className="icono-twitter"> </a>
+                    </div>
+                </div>
             </div>
-            <div className="caja">
-
+            <div className="contenedor-2">
+                <small>&copy; 2023 <b>Heroe Wear</b> - Todos los Derechos son Reservados </small>
             </div>
-            <div className="caja">
-
-            </div>
+        <style jsx>{`
+        .contenedor-footer{
+            width: 100%;
+            background-color: #252629;
+        }
+        
+        .contenedor-1{
+            width: 100%;
+            max-width: 1200px;
+            margin: auto;
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 50px;
+            padding: 45px 0px;
+        }
+        `}</style>
         </div>
+        
+        
     );
 }
 
