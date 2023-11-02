240f8b0a1a66ff5ec5281d23ba5cebd9c78715af
Maurot31 <maurotoledo031@gmail.com>
2023-11-02 17:23:27 -0300

    Add: como salió jajaja

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -1,14 +1,11 @@
-import AddButton from './AddButton'
+import AddButton from "./AddButton";
 import Image from "next/image";
 
-
 const Product = ({ product, addToCart }) => {
-
-  const { id, name, price, imageUrl } = product
+  const { id, name, price, imageUrl } = product;
 
   return (
     <>
-
       <div className="product">
         <Image src={imageUrl} width={200} height={200}></Image>
         <h3>{name}</h3>
@@ -16,42 +13,38 @@ const Product = ({ product, addToCart }) => {
         <AddButton id={id} addToCart={addToCart} />
       </div>
 
-
       <style jsx>{`
-            
-                
-                .product {
-                  margin: 20px;
-                  max-width: 200px;
-                  display: flex;
-                  flex-direction: column;
-                  align-items: center;
-                }
-                
-                .product img {
-                  max-width: 100%;
-                  height: auto;
-                  border: 1px solid #ccc;
-                  border-radius: 8px;
-                }
-                
-                h3{
-                  margin: 10px 0;
-                  color:#FFFFFF;
-                  font-size: 18px;
-                }
-            
-                
-                p {
-                  font-size: 16px;
-                  font-weight: bold;
-                  color: #007bff;
-                  margin: 0;
-                  margin-bottom: 10px;
-                }
-            `}</style>
-    </>
-  )
-}
+        .product {
+          margin: 20px;
+          max-width: 200px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
 
-export default Product
+        .product img {
+          max-width: 100%;
+          height: auto;
+          border: 1px solid #ccc;
+          border-radius: 8px;
+        }
+
+        h3 {
+          margin: 10px 0;
+          color: #ffffff;
+          font-size: 18px;
+        }
+
+        p {
+          font-size: 16px;
+          font-weight: bold;
+          color: #007bff;
+          margin: 0;
+          margin-bottom: 10px;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Product;
